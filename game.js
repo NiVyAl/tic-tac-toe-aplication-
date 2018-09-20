833de1012693648e2759526a81f0ddfd1afc2bf4
nick <nikitavisoc@gmail.com>
2018-09-20 22:34:26 +0300

    1.25 chessfigure

--- game.js
+++ game.js
@@ -12,7 +12,14 @@ var clickControl = function(control, num){
                                 hod(control, num);
                              })
     }
-    
+
+var cheesman = function(text, player) {
+    if (player == 1) {
+        h1.innerHTML = text + " <span class='cheesman color-" + player + "' > X</span>";
+    } else if (player == 2) {
+        h1.innerHTML = text + " <span class='cheesman color-" + player + "' >O</span>";
+    }
+}     
 
 var hod = function(adress, num){
     
@@ -20,7 +27,8 @@ var hod = function(adress, num){
 
         if (player == 1){
             adress.innerHTML = 'x';
-            h1.innerHTML = 'Ход игрока 2';
+            //h1.innerHTML = 'Ход игрока 2';
+            cheesman('Ход игрока', 2);
             player1.classList.remove('h2-active');
             player2.classList.add('h2-active');
             adress.classList.add('color-1')
@@ -32,7 +40,8 @@ var hod = function(adress, num){
             adress.innerHTML = 'O';
             player2.classList.remove('h2-active');
             player1.classList.add('h2-active');
-            h1.innerHTML = 'Ход игрока 1';
+            //h1.innerHTML = 'Ход игрока 1';
+            cheesman('Ход игрока', 1);
             adress.classList.add('color-2');
             player = 1;
             mas[num] = 5;
@@ -63,7 +72,6 @@ var isWin = function(){
         }    
     };
     
-    console.log(mas[0] + mas[4] + mas[8]);
     if (mas[0] + mas[4] + mas[8] == 9) {
         winCongratulations(1);
     }; 
@@ -82,7 +90,6 @@ var isWin = function(){
 
 for (var i=0; i< td.length; i++){
     clickControl(td[i], i);
-    
 }
 
 var restart = function() {
@@ -90,7 +97,11 @@ var restart = function() {
 }
 
 var winCongratulations = function(winer) {
-    h1.innerHTML = "Победил игрок " + winer;
+    if (winer == 1) {
+        h1.innerHTML = "Победил игрок <span class='cheesman color-" + winer + "' >X</span>";
+    } else if (winer == 2) {
+        h1.innerHTML = "Победил игрок <span class='cheesman color-" + winer + "' >O</span>";
+    }
     h1.classList.add("win-message");
     document.querySelector("table").classList.add("end-math");
 }
